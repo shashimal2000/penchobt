838fde84cdd344f246e48cf362130ca978c75382
shashimal2000 <115067658+shashimal2000@users.noreply.github.com>
2025-05-27 17:21:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "P4IkCJ4Y#4_6ho_id9g8WKj_BO11AMoXhyXQ73MrHKxN2VUY5L4I",
   OWNER_NUM: process.env.OWNER_NUM || "94788779949",
+  PREFIX: process.env.PREFIX || ".",
 };
