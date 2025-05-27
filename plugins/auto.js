1dce1699b8ea7c634a3e568b7c5f5daf635bd1ff
shashimal2000 <115067658+shashimal2000@users.noreply.github.com>
2025-05-27 18:46:40 +0530

    Update auto.js

--- plugins/auto.js
+++ plugins/auto.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const {readEnv} = require('../lib/database')
+const config = require('../config')
 const {cmd , commands} = require('../command')
 
 //auto reply 
@@ -8,11 +8,11 @@ cmd({
   on: "body"
 },    
 async (robin, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autoreply.json');
+    const filePath = path.join(__dirname, '../data/autoreply.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+  
             if (config.AUTO_REPLY === 'true') {
                 //if (isOwner) return;        
                 await m.reply(data[text])
