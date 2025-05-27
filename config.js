d1ff59cadee081621668ed35df7cb32d49cdb71c
shashimal2000 <115067658+shashimal2000@users.noreply.github.com>
2025-05-27 13:26:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "P4IkCJ4Y#4_6ho_id9g8WKj_BO11AMoXhyXQ73MrHKxN2VUY5L4I",
   MONGODB: process.env.MONGODB || "mongodb+srv://bota:bota@cluster0.tlvth93.mongodb.net/",
   OWNER_NUM: process.env.OWNER_NUM || "94788779949",
 };
