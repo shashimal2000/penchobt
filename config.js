c23aa3608da547468b0efa89c8442caf0528e9bd
shashimal2000 <115067658+shashimal2000@users.noreply.github.com>
2025-05-28 13:09:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "P4IkCJ4Y#4_6ho_id9g8WKj_BO11AMoXhyXQ73MrHKxN2VUY5L4I",
-  OWNER_NUM: process.env.OWNER_NUM || "94788779949",
+  SESSION_ID: process.env.SESSION_ID || "P04CxTyR#s6-IrvjjJmPRRj43EymGCtB-50dMyvS__6IsjbapX8s",
+  OWNER_NUM: process.env.OWNER_NUM || "94757501723",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20iz%20alive.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am alive now ",
